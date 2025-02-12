2178393ad14a6443e69f3b46f8c81650b2f0efc6
pie240 <andyalmeida191@gmail.com>
2025-02-11 22:18:01 -0800

    [UPDATE] Slowly coming together

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Preloader from "./components/Pre";
 import ScrollToTop from './components/ScrollToTop';
 import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
 import {
   BrowserRouter as Router,
   Route,
@@ -14,14 +15,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+    }, []);
+
   return (
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <NavBar />
+        <Navbar />
         <ScrollToTop />
         <Routes>
-
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
